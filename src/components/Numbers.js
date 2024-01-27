8d9736ef76553fd82a94b3a9cfce5d0654dd0805
Felipe Rezende <f.zende@gmail.com>
2024-01-27 14:47:24 +0100

    done

---
+++ src/components/Numbers.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Buttons/Button'
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const renderButtons = onClickNumber => {
+
+    const renderButton = number => (
+        <Button 
+            key={number} 
+            text={number.toString()} 
+            clickHandler={onClickNumber} 
+        />
+    )
+    return numbers.map(renderButton)
+}
+
+const Numbers = ({ onClickNumber }) => (
+    <section className="numbers">
+        {
+            renderButtons(onClickNumber)
+        }
+    </section>
+)
+
+Numbers.propTypes = {
+    onClickNumber: PropTypes.func.isRequired
+}
+
+export default Numbers;
